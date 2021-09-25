56ccd10fef537125a37593737c3f79928721ef3e
Kai Oswald <kai.oswald@creative-software.de>
2021-09-25 14:01:23 +0200

    Persist configuration in localStorage

--- src/store.js
+++ src/store.js
@@ -1,15 +1,25 @@
 import { reactive } from "@vue/reactivity"
 import { quadrants, rings } from "@/data/defaults.js";
 
+
+let local = {
+    quadrants: JSON.parse(localStorage.getItem("quadrants")),
+    rings: JSON.parse(localStorage.getItem("rings"))
+};
+
 export const store = {
     state: reactive({
-        quadrants,
-        rings,
+        quadrants: local.quadrants ?? quadrants,
+        rings: local.rings ?? rings,
     }),
     setQuadrants(newVal) {
         this.state.quadrants = newVal;
+        // persist to localStorage, so we can retrieve the data after hard reload
+        localStorage.setItem("quadrants", JSON.stringify(newVal));
     },
     setRings(newVal) {
         this.state.rings = newVal;
+        // persist to localStorage, so we can retrieve the data after hard reload
+        localStorage.setItem("rings", JSON.stringify(newVal));
     }
 }
